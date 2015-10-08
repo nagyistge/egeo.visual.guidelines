2c2540200d035b5a47dc7d1a739af6867d749609
alexrchies <alexrchies@gmail.com>
2015-10-08 09:51:03 +0200

    fix(bat): fix some errors

--- generate-doc.bat
+++ generate-doc.bat
@@ -1,4 +1,4 @@
-rm -R dist/styleguide
-.\node_modules\.bin\kss-node --source src --destination dist --template vendors/kss-template --homepage readme.md --css ../styleguide.css
+rm -R dist
+.\node_modules\.bin\kss-node --source src --destination dist --template node_modules/egeo.website.template/dist/kss-template --homepage readme.md --css public/styleguide.css';
 pause
  
